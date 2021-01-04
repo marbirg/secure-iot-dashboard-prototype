73a08165948109fca7956ab87f57e805c527c403
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2021-01-04 16:58:43 +0100

    add possibility to use config file, set random suffix on name for Thing to avoid collission

--- python-wot/main.py
+++ python-wot/main.py
@@ -18,28 +18,40 @@ import time
 import tornado.ioloop
 import uuid
 
+import json
+import os
+
+DEFAULT_CONF = {
+    "name":"patient",
+    "readers":["patient"]
+}
+CONFIG_FILE = os.getenv("CONFIG_FILE", "config.json")
+def readConfig(fname):
+    try:
+        with open(fname) as json_data_file:
+            data = json.load(json_data_file)
+        return data
+    except Exception as e:
+        print("ERROR:",e)
+        return DEFAULT_CONF
 
 def run_server():
-    # Create a thing that represents a dimmable light
-    # light = ExampleDimmableLight()
-
-    # Create a thing that represents a humidity sensor
-    # sensor = FakeGpioHumiditySensor()
-
-    # Create Cab Companies:
-    # patientA = CabCompany('PatientA', nCars=2)
-
+    data = readConfig(CONFIG_FILE)
+    name = data['name']
+    readers = data['readers']
     # Create DLM:
-    label1 = DLM()
-
-    pulseSensor = FakePulseSensor()
+    dlm = DLM()
+    dlm.addPolicy(name, readers)
+    pulseSensor = FakePulseSensor(classification=dlm)
 
     sensors = [pulseSensor]
     # If adding more than one thing, use MultipleThings() with a name.
     # In the single thing case, the thing's name will be broadcast.
-    server = WebThingServer(MultipleThings(sensors,
-                                           'Thermometers'),
-                            hostname='wot', port=8888, debug=True)
+    hostname = 'wot'#+str(random.randint(0,1000))
+    endpoint_name = 'pulsesensor'+str(random.randint(0,1000))
+    # print("Hostname:",hostname)
+    server = WebThingServer(MultipleThings(sensors,endpoint_name),
+                            hostname=hostname, port=8888, debug=True)
     try:
         logging.info('starting the server')
         server.start()
