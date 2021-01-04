0fb78f2fcebf0c4ea57dad3be92a6d55e58cb08c
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2021-01-04 16:55:20 +0100

    add minimal implementation for dlm model

--- python-wot/security/dlm.py
+++ python-wot/security/dlm.py
@@ -1,7 +1,25 @@
 
+import json
 
 
-class DLM:
+class DLM(dict):
+    _instance_number = 0
 
     def __init__(self):
-        print("New DLM model")
+        type(self)._instance_number+=1
+        print("New DLM model instance=",type(self)._instance_number)
+        dict.__init__(self)
+        # self['Instance']=type(self)._instance_number
+        # print("My state:",self)
+
+    def addPolicy(self, name, readers=[]):
+        self[name]=readers
+
+    # def default(self, obj):
+    #     print("Calling default method!!!")
+    #     return dict(_class_object={})
+    #     return json.dumps({})
+    
+    # def toJSON(self):
+    #     return json.dumps(self, default=lambda o: o.__dict__, 
+    #         sort_keys=True, indent=4)
