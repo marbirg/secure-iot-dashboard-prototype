a3142287aa38ea11cf2291046b3463d33c0e63a8
Marcus Schmidt Birgersson <marbir@kth.se>
2021-08-20 14:49:33 +0200

    update mongo init script to create smart city database as well

--- data/init-mongo.js
+++ data/init-mongo.js
@@ -15,3 +15,22 @@ db.createUser({
 
 db.createCollection('devices', { capped: false });
 db.createCollection('properties', { capped: false });
+
+
+db = new Mongo().getDB("smart-city");
+
+db.createUser({
+  user: 'nodered',
+  pwd: 'nodered',
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'smart-city',
+    },
+  ],
+});
+
+
+
+db.createCollection('devices', { capped: false });
+db.createCollection('properties', { capped: false });
